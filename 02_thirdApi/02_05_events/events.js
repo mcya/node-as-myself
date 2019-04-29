1d69422b986aff3680ef8c7232e5b50f5008cc25
mcya <1027057875@qq.com>
2019-04-29 17:27:04 +0800

    05 events

--- 02_thirdApi/02_05_events/events.js
+++ 02_thirdApi/02_05_events/events.js
@@ -9,19 +9,19 @@ var eventEmitter = new events.EventEmitter();
 // 绑定事件及事件的处理程序
 eventEmitter.on('connection', function(){
 	setTimeout(function(){
-		console.log('连接成功。');
+		console.log('log: 连接成功。');
 	}, 1000);
-	
-	// 触发 data_received 事件 
+
+	// 触发 data_received 事件
 	eventEmitter.emit('data_received');
 });
 
 //trigger => emit
 // 使用匿名函数绑定 data_received 事件
 eventEmitter.on('data_received', function(){
-	console.log('数据接收成功。');
+	console.log('log: 数据接收成功。');
 });
 
 //用 eventEmitter 对象的 emit 方法来调用事件
 eventEmitter.emit('connection');
-console.log("程序执行完毕。");
+console.log("log: 程序执行完毕。");
