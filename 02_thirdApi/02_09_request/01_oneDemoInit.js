365ef865e3f3487b28e664311c054152ff5d6cf1
mcya <1027057875@qq.com>
2019-05-06 15:36:23 +0800

    var request = require('request');

--- 02_thirdApi/02_09_request/01_oneDemoInit.js
+++ 02_thirdApi/02_09_request/01_oneDemoInit.js
@@ -1,16 +1,7 @@
-# request 爬虫 (cheerio+request) #
-
-[request 官方文档](https://www.npmjs.com/package/request)
-[cheerio GitHub](https://github.com/cheeriojs/cheerio)
-
-### 安装
-`npm install request`
-
-```js
 var request = require('request');
+
 request('http://www.baidu.com', function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
 });
-```
