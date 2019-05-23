4cf3fb568f6b3312655a835bd4e3154f5703e0cf
mcya <1027057875@qq.com>
2019-05-23 15:11:51 +0800

    fs

--- 02_06_fs/02-write.js
+++ 02_06_fs/02-write.js
@@ -7,14 +7,14 @@ var fs = require('fs');
 //writeFiel 是覆盖写入文本
 fs.writeFile('input.txt', '抵制一切不利于中国和世界和平的动机',  function(err) {
    if (err) {
-       return console.error(err);
+      return console.error(err);
    }
    console.log("数据写入成功！");
    console.log("--------我是分割线-------------")
    console.log("读取写入的数据！");
    fs.readFile('input.txt', function (err, data) {
       if (err) {
-         return console.error(err);
+        return console.error(err);
       }
       console.log("异步读取文件数据: " + data.toString());
    });
