c26a221c4959657e881371c83856a24e259801ca
mcya <1027057875@qq.com>
2019-05-27 15:04:49 +0800

    body-parser

--- 04_01_express/02_post.js
+++ 04_01_express/02_post.js
@@ -5,9 +5,6 @@ app.listen(8083, function(){
 	console.log('Server running on http://localhost:8083');
 });
 
-// app.get('/', function(request, response){
-//   response.send('this is post demo');
-// })
 
 
 // ---------------------------------------------------------------------------------------------------------
@@ -15,14 +12,16 @@ app.listen(8083, function(){
 // ---------------------------------------------------------------------------------------------------------
 
 // 参数接收和 GET 基本一样，不同的在于 GET 是 `request.query` 而 POST 的是 `request.body`
-// 访问地址：`http://localhost:8083/getParams?username=dk&age=18`，可通过 `request.query` 来获取参数
 var bodyParser = require('body-parser');
 // 创建 application/x-www-form-urlencoded 编码解析
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
-app.post('/getParams', urlencodedParser, function (request, response) {
+app.post('/postParams', urlencodedParser, function (request, response) {
     var params = {
+				falge: true,
         username: request.body.username,
         age: request.body.age
     }
    response.send(params);
 });
+
+// 关于post的使用需要 结合 HTML 来实现 即是执行一个方法 - 02_post.html
