77edeb1c04a9207a38debe3e8f9bf0fbcfa3d7dd
mcya <1027057875@qq.com>
2019-05-27 14:51:10 +0800

    body-parser

--- 04_01_express/02_post.js
+++ 04_01_express/02_post.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.listen(8080, function(){
-	console.log('Server running on http://localhost:8080');
+app.listen(8083, function(){
+	console.log('Server running on http://localhost:8083');
 });
 
-app.get('/', function(request, response){
-  response.send('this is post demo');
-})
+// app.get('/', function(request, response){
+//   response.send('this is post demo');
+// })
 
 
 // ---------------------------------------------------------------------------------------------------------
@@ -15,7 +15,7 @@ app.get('/', function(request, response){
 // ---------------------------------------------------------------------------------------------------------
 
 // 参数接收和 GET 基本一样，不同的在于 GET 是 `request.query` 而 POST 的是 `request.body`
-// 访问地址：`http://localhost:8080/getParams?username=dk&age=18`，可通过 `request.query` 来获取参数
+// 访问地址：`http://localhost:8083/getParams?username=dk&age=18`，可通过 `request.query` 来获取参数
 var bodyParser = require('body-parser');
 // 创建 application/x-www-form-urlencoded 编码解析
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
