340e4f3b737a75647cfa405130a1b1f2478b39c9
mcya <1027057875@qq.com>
2019-07-22 17:07:36 +0800

    express all don`t k

---
+++ 04_04_express_all/01_startMongodb.js
@@ -0,0 +1,22 @@
+// var mongodb = require('mongodb');
+// var MongoClient = mongodb.MongoClient;
+var db;
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'myproject';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  client.close();
+});
